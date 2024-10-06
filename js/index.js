abd61c36407bb2f00aa34db9916a3c6115619912
rasel mridha <raselmridha792@gmail.com>
2024-10-06 22:31:03 +0600

    creating the adopted button modal and make the button disabled by using button.disable = true method

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ const displayData = (pets) => {
                           </div>
                           <div class="grid grid-cols-3 lg:grid-cols-3 md:grid-cols-2 gap-2 item-center justify-between mt-5">
                             <button id="like-button" onclick="displayImage(${petId})" class="btn btn-outline btn-accent "><i class="fa-regular fa-thumbs-up text-teal-700 border-teal-800"></i></button>
-                            <button class="btn btn-outline btn-accent">Adopt</button>
+                            <button  class="btn btn-outline btn-accent" onclick="openModal(this)">Adopt</button>
                             <button class="btn btn-outline btn-accent" onclick="showModalBox(${petId})">Details</button>
                           </div>
                         </div>
@@ -145,43 +145,44 @@ const showImage = (pet) => {
 }
 
 // show modal 
-const showModalBox = async (id)=>{
+const showModalBox = async (id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
     const data = await response.json();
     displayModal(data.petData)
 }
 
-const displayModal = (data) =>{
-    const {petId, breed, category, date_of_birth, price, image, gender, pet_details, vaccinated_status, pet_name} = data;
+const displayModal = (data) => {
+    const { petId, breed, category, date_of_birth, price, image, gender, pet_details, vaccinated_status, pet_name } = data;
     const modalBox = document.getElementById('detailsModal');
     modalBox.innerHTML = '';
     const div = document.createElement('div');
     div.innerHTML = `
                 <div class="modal-box w-11/12 max-w-5xl">
-                    <img class="p-10 w-full rounded" src="${image}" alt="">
+                    <img class="p-10 w-full border rounded-xl" src="${image}" alt="">
                     <h2 class="card-title text-2xl my-2">${pet_name ? pet_name : 'not Available'}</h2>
                 <div class="text-gray-500">
                     <div class="flex flex-col md:flex-row md:gap-5">
                         <div>
-                            <p class="flex gap-2"><img class="w-5 max-h-5" src="https://img.icons8.com/?size=100&id=8gmS8B8ERDZk&format=png&color=000000" /> Breed:
+                            <p class="flex gap-3"><img class="w-5 max-h-5" src="https://img.icons8.com/?size=100&id=8gmS8B8ERDZk&format=png&color=000000" /> Breed:
                                 ${breed ? breed : 'not Available'}</p>
-                            <p class="flex gap-2"><img class="w-5 max-h-5"
-                                    src="https://img.icons8.com/?size=100&id=89201&format=png&color=000000" /> Birth:
-                                ${date_of_birth ? date_of_birth : 'not available'}</p>
-                        </div>
-                        <div>
-                            <p class="flex gap-2"><img class="w-5 max-h-5"
+                            <p class="flex gap-3"><img class="w-5 max-h-5"
                                     src="https://img.icons8.com/?size=100&id=1665&format=png&color=000000" /> Gender:
                                 ${gender ? gender : 'not Available'}</p>
-                            <p class="flex gap-2"><img class="w-5 max-h-5"
+                        </div>
+                        <div>
+                            <p class="flex gap-3"><img class="w-5 max-h-5"
+                                    src="https://img.icons8.com/?size=100&id=89201&format=png&color=000000" /> Birth:
+                                ${date_of_birth ? date_of_birth : 'not available'}</p>
+
+                            <p class="flex gap-3"><img class="w-5 max-h-5"
                                     src="https://img.icons8.com/?size=100&id=2971&format=png&color=000000" /> Price: ${price
-                                ? price : 'not Available'}</p>
+            ? price : 'not Available'}</p>
                         </div>
                     </div>
-                    <p class="flex gap-2"><img class="w-5 max-h-5"
-                            src="https://img.icons8.com/?size=100&id=2971&format=png&color=000000" /> Price: ${price ?
-                        price : 'not Available'}</p>
-                    <hr class="my-5" />
+                    <p class="flex gap-2 py-2"><img class="w-5 max-h-5"
+                                    src="https://img.icons8.com/?size=100&id=1665&format=png&color=000000" /> Vaccination:
+                                ${vaccinated_status ? vaccinated_status : 'not Available'}</p>
+                    <hr class="py-5"/>
                 </div>
                 <div>
                     <h1 class="text-xl font-bold">details Information</h1>
@@ -196,22 +197,43 @@ const displayModal = (data) =>{
             </div>
     `;
     modalBox.appendChild(div);
-
-    
     // showing the modal by clicking 
     modalBox.showModal();
+
+    // for disabled the button 
 }
 
 
-// {
-//     "petId": 4,
-//     "breed": "Holland Lop",
-//     "category": "Rabbit",
-//     "date_of_birth": "2023-06-30",
-//     "price": 200,
-//     "image": "https://i.ibb.co.com/4g3Jrjf/pet-4.jpg",
-//     "gender": "Female",
-//     "pet_details": "This adorable female Holland Lop rabbit, born on June 30, 2023, is known for her calm and gentle nature. She thrives in quiet environments and enjoys being handled with care. Priced at $200, she is an ideal pet for those looking for a low-maintenance, friendly rabbit. Note that she is not vaccinated.",
-//     "vaccinated_status": "Not",
-//     "pet_name": "Nibbles"
-// }
+
+// for coundown modal 
+
+const openModal = (buttonElement) => {
+    const modalbody = document.getElementById('adoptModal');
+    const countdownElement = document.getElementById('countdown');
+    let countdown = 3;
+    modalbody.showModal();
+
+    const updateCountDown = () => {
+        countdownElement.textContent = countdown;
+        // set a condition for coundown 
+        if (countdown > 0) {
+            countdown--;
+
+            setTimeout(updateCountDown, 1000);
+        } else {
+            modalbody.close();
+        }
+    }
+    updateCountDown();
+
+    // calling the pet id for disabling 
+    buttonDisable(buttonElement)
+}
+
+// for disable the button 
+const buttonDisable =(buttonElement) =>{
+    // const button = document.getElementById('disable-adopt-btn');
+    console.log(buttonElement);
+    buttonElement.disabled = true;
+    buttonElement.innerText = 'Adopted'
+}
